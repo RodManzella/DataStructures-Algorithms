dd0f51f616faefc916fb5d495072e9addbf3ac2a
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-03-18 22:19:54 -0300

    a

--- BinarySearchTree/src/BinarySearchTree.java
+++ BinarySearchTree/src/BinarySearchTree.java
@@ -11,7 +11,18 @@ public class BinarySearchTree {
     private BinaryNode insert(BinaryNode currentNode, int value){
         if(currentNode == null){
             BinaryNode newNode = new BinaryNode();
-            newNode.setValue(value);
+            newNode.value = value;
+            return newNode;
+        }else if(value <= currentNode.value){
+            currentNode.left = insert(currentNode.left, value);
+            return currentNode;
+        }else{
+            currentNode.right = insert(currentNode.right, value);
+            return currentNode;
         }
+    }
+
+    public void insert(int value){
+        insert(root, value);
     }
 }
